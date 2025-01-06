4f83357b3e2d0687f1b299e7e40e5d925709d106
jc-rsn <110260774+jc-rsn@users.noreply.github.com>
2025-01-06 23:59:40 +0400

    implemented with dataclasses + refactor

--- snowflake/main.py
+++ snowflake/main.py
@@ -1,22 +1,30 @@
 import time
 from dataclasses import dataclass
 from typing import ClassVar
-
 from fastapi import FastAPI, APIRouter
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'Hello': 'World'}
+
 
 snowflake = APIRouter(
-    prefix="/snowflake",
-    tags=["snowflake"]
+    prefix='/snowflake',
+    tags=['snowflake']
 )
 
 
-@snowflake.get("/snowflake")
-async def get_snowflake():
+@snowflake.get('/id')
+async def get_id():
     return create_snowflake()
 
 
 @dataclass(frozen=True)
-class SnowflakeComponents:
+class SnowflakeConstant:
     base_epoch: int = 0
     max_timestamp: int = int(0b11111111111111111111111111111111111111111)
     max_node: int = int(0b1111111111)
@@ -30,24 +38,24 @@ class SnowflakeState:
 
 
 def create_snowflake() -> int:
-    node = SnowflakeComponents.max_node
-    epoch = SnowflakeComponents.base_epoch
+    node = SnowflakeConstant.max_node
+    epoch = SnowflakeConstant.base_epoch
     timestamp = int(time.time() * 1000) - epoch
 
     if epoch < 0:
-        raise ValueError("epoch cannot be negative")
+        raise ValueError('epoch cannot be negative')
 
-    if node < 0 or node > SnowflakeComponents.max_node:
-        raise ValueError(f"node cannot be negative and must be less than {SnowflakeComponents.max_node}")
+    if node < 0 or node > SnowflakeConstant.max_node:
+        raise ValueError(f'node cannot be negative and must be less than {SnowflakeConstant.max_node}')
 
-    if timestamp < 0 or timestamp > SnowflakeComponents.max_timestamp:
-        raise ValueError(f"timestamp cannot be negative and must be less than {SnowflakeComponents.max_timestamp}")
+    if timestamp < 0 or timestamp > SnowflakeConstant.max_timestamp:
+        raise ValueError(f'timestamp cannot be negative and must be less than {SnowflakeConstant.max_timestamp}')
 
     if timestamp == SnowflakeState.last_timestamp:
-        SnowflakeState.last_sequence = (SnowflakeState.last_sequence + 1) & SnowflakeComponents.max_sequence
+        SnowflakeState.last_sequence = (SnowflakeState.last_sequence + 1) & SnowflakeConstant.max_sequence
 
         if SnowflakeState.last_sequence == 0:
-            raise ValueError(f"sequence cannot be negative and must be less than {SnowflakeComponents.max_sequence}")
+            raise ValueError(f'sequence cannot be negative and must be less than {SnowflakeConstant.max_sequence}')
     else:
         SnowflakeState.last_sequence = 0
         SnowflakeState.last_timestamp = timestamp
@@ -58,3 +66,7 @@ def create_snowflake() -> int:
 
 
 __all__ = ['create_snowflake']
+
+if __name__ == '__main__':
+    app.include_router(snowflake)
+    uvicorn.run(app=app)
